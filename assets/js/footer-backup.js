06b2ad504feff5724baafd290c6af078f8ce293e
Alex - Webkonsulenter <alex@webkonsulenter.dk>
2021-02-18 20:30:45 +0800

    footer js backup

--- assets/js/footer-backup.js
+++ assets/js/footer-backup.js
@@ -56,80 +56,33 @@ jQuery(window).on('load', function () {
 	section1Height = jQuery(section1).height();
 	console.log('loaded #section1 height is: ' + section1Height);
 });
-window.fromPosition = 0; // in multiples of windowHeight. i.e. what section was viewed last
+
 jQuery(window).scroll(function (event) {
 	var scrollPosition = jQuery(window).scrollTop();
-	
-	//console.log('scroll positition is: ' + scrollPosition);
+	console.log('scroll positition is: ' + scrollPosition);
 	// reset to no animation:
 	if (scrollPosition < windowHeight) {
-		jQuery(vehicle1).css('transform', 'translateX(0)');
-		var fromPosition = 0;
+		jQuery(vehicle1).css('transform', 'none')
 	}
 	if ((scrollPosition >= windowHeight) && (scrollPosition < 2 * windowHeight)) {
-		//jQuery(vehicle1).css('transform', 'rotate(90deg)');
-		
-		var fromPosition = 1;
+		jQuery(vehicle1).css('transform', 'rotate(90deg)');
 	}
 	if ((scrollPosition >= 2 * windowHeight) && (scrollPosition < 3 * windowHeight)) {
 		// jQuery(vehicle1).css('transition', 'ease-in-out transform 1s');
-		
-		
-		var fromPosition = 2;
+		jQuery(vehicle1).css('transition', 'ease-in-out all 1s');
+		jQuery(vehicle1).css('transform', 'translateX(-60vw) rotate(90deg)');
+		jQuery(vehicle2).css('transform', 'translateX(-60vw)');
 	}
-	if ((scrollPosition >= 3 * windowHeight) && (scrollPosition < 4 * windowHeight && window.fromPosition != 3)) {
-		console.log(window.fromPosition);
-		window.fromPosition = 3;
-		jQuery(vehicle1).css('transition', 'ease transform 0.01s');
-		//jQuery(vehicle1).css('transition', 'ease-in-out all 1s');
-		jQuery(vehicle1).animate({rotation: 90}, {
-  		  duration: 500,
- 		   easing: 'linear',
- 		   step: function () {
-			   yPos = 0.3*(jQuery('#vehicle1').height()/90)*this.rotation;
-			   vehicle1.css({transform: 'rotate(' + this.rotation + 'deg) translateY('+yPos+'px)'});
-				jQuery(vehicle2).css('transform', 'translateX(-60vw)');
-    		},
-			complete: function(){
-				jQuery(vehicle2section).css('visibility', 'visible');
-			}
-		});
-		
-		jQuery(vehicle1).animate({translation: -60}, {
-  		  duration: 1000,
- 		   easing: 'linear',
- 		   step: function () {
-  		      //var yPos = vehicle1.css('transform').split(/[()]/)[1].split(',')[5];
-			   vehicle1.css({transform: 'translateX(' + this.translation + 'vw) rotate(90deg) translateY('+yPos+'px)'});
-			   vehicle2.css({transform: 'translateX(' + Math.abs(this.translation) + 'vw)'});
-    		},
-			complete: function(){
-				vehicle1.css({display: 'none'});
-				vehicle1.stop(true,true);
-				vehicle2.stop(true,true);
-			}
-		});
-		
-		//jQuery(vehicle1).css('transform', 'translateX(-60vw) rotate(90deg)');
-		
-		//jQuery(vehicle1).css('display', 'none');
-		
+	if ((scrollPosition >= 3 * windowHeight) && (scrollPosition < 4 * windowHeight)) {
+		// jQuery(vehicle1).css('transition', 'ease-in-out transform 1s');
+		jQuery(vehicle1).css('display', 'none');
+		jQuery(vehicle2section).css('visibility', 'visible');
+		jQuery(vehicle2).css('transform', 'translateX(0)');
 	}
 	if ((scrollPosition >= 4 * windowHeight) && (scrollPosition < 5 * windowHeight)) {
-		// jQuery(vehicle1).css('transition', 'ease-in-out transform 1s');
-		jQuery(vehicle2section).css('visibility', 'visible');
-			jQuery(vehicle2section).css('visibility', 'visible');
-		jQuery(vehicle2).css('transform', 'translateX(0)');
-		
-		var fromPosition = 4;
-	}
-	if ((scrollPosition >= 6 * windowHeight) && (scrollPosition < 7 * windowHeight)) {
 		// jQuery(vehicle1).css('transition', 'ease-in-out transform 1s');
 		jQuery(vehicle2).css('display', 'none');
 		jQuery(vehicle2).css('position', 'relative');
 		jQuery(vehicle3section).css('visibility', 'visible');
-		
-		var fromPosition = 5;
 	}
 });
-console.log(jQuery('#vehicle1').width());
